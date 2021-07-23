e08e0090bfbe90732555f48f15f534d33a1eaf14
Akbarjon <tojiyev.akbarjon@gmail.com>
2021-07-23 20:43:49 +0500

    Add minor

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const Bot = require('node-telegram-bot-api')
 const bot = new Bot(process.env.TOKEN, { polling: true })
 
 bot.onText(/\/start/, msg => {
-	console.log(msg)
+	bot.sendMessage(msg.from.id, `Assalomu alaykum ${msg.from.first_name} 🤓\nUyga vazifangizni menga yuboravering, faqat Ustozga ko'rsataman xolos 😅`)
 })
 
 bot.on('message', msg => {
-	console.log(msg)
-
-	bot.forwardMessage('-524111235', msg.from.id, msg.message_id)
+	if(msg.document) {
+		bot.forwardMessage('-524111235', msg.from.id, msg.message_id)
+	}
 })
